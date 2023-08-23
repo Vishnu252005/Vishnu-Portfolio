aabcf9bb5890283b767f9e73844b3be93a7c22f8
Vishnu S <141705372+Vishnu252005@users.noreply.github.com>
2023-08-24 00:12:43 +0530

    script.js

--- projects/script.js
+++ projects/script.js
@@ -32,7 +32,7 @@ document.addEventListener('visibilitychange',
 
 // fetch projects start
 function getProjects() {
-    return fetch("projects.json")
+    return fetch("./projects/projects.json")
         .then(response => response.json())
         .then(data => {
             return data
