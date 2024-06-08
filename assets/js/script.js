70f84d2e174e0c6afe4d0d3c17699b8b63fe7d2d
Vishnu252005 <vishnu1252005@gmail.com>
2024-06-08 19:26:57 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("user_eRNqe9ljeSELqpQnX");
 
         emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
             .then(function (response) {
